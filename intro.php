4a6a575671d8d07dd01be9b4e5d18836c0fe8d72
Dennis Kamau <dmkamau475@gmail.com>
2021-03-02 17:41:50 +0300

    first commit

---
+++ intro.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dennis PHP</title>
+</head>
+<body>
+
+<h1>This is my first PHP File!</h1>
+<?php
+
+echo "Hello PHP world";
+echo "<br>";
+echo "Hi there, am Dennis Kamau. Its nice to meet you.";
+echo "<br>";
+echo "This is my first PHP site!";
+echo "<br>";
+
+?>
+
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Variables</title>
+</head>
+<body>
+
+<?php
+
+$Firstname = "Dennis";
+$Secondname = "Kamau";
+
+$num1 = 30;
+$num2 = 74;
+
+echo $Firstname;
+echo "<br>";
+echo $Secondname;
+echo "<br>";
+echo  $num1;
+echo "<br>";
+echo $num2;
+
+?>
+
+</body>
+</html>
